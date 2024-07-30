fcaf563556acf9d0d3a54d9a43f480be1c320d88
Mihai <mihai.chirculescu@yahoo.com>
2024-07-30 23:48:26 +0300

    Bug fix

--- app.py
+++ app.py
@@ -606,7 +606,7 @@ async def get(session, app, request):
     if base_link not in request.url.path:
         add_toast(session, f"Please use the following link: {base_link}", "info")
         
-    return container wrapper
+    return container_wrapper
 
 @rt('/stats')
 async def get(session, app, request):
