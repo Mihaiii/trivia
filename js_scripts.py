97f5ef49f17273ca83fa01ca305df0a9d25810a0
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2025-04-04 22:36:30 +0300

    Update js_scripts.py

--- js_scripts.py
+++ js_scripts.py
@@ -3,7 +3,8 @@ from fasthtml.xtend import Script
 
 def ThemeSwitch(sel='#theme-toggle', buttonClass = 'secondary'):
     src = """
-import { proc_htmx} from "https://cdn.jsdelivr.net/gh/answerdotai/fasthtml-js@1.0.5/fasthtml.js";
+import { proc_htmx} from "https://cdn.jsdelivr.net/gh/answerdotai/fasthtml-js@1.0.3/fasthtml.js";
+document.addEventListener("DOMContentLoaded", () => {
 
 const button = document.createElement('button');
 button.id = 'theme-switch';
@@ -95,7 +96,7 @@ me(selector).on("click", _ =>
     localStorage.setItem("triviaTheme", newTheme)
     setIconToShow()
 })
-
+});
 """ % (buttonClass, sel)
     return Script(src, type='module')
 
