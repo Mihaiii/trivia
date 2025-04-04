a8af91a1d18b6b6bd8f942f020d49148fadc1d65
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2025-04-04 22:16:58 +0300

    Update js_scripts.py

--- js_scripts.py
+++ js_scripts.py
@@ -3,7 +3,7 @@ from fasthtml.xtend import Script
 
 def ThemeSwitch(sel='#theme-toggle', buttonClass = 'secondary'):
     src = """
-import { proc_htmx} from "https://cdn.jsdelivr.net/gh/answerdotai/fasthtml-js/fasthtml.js";
+import { proc_htmx} from "https://cdn.jsdelivr.net/gh/answerdotai/fasthtml-js@1.0.5/fasthtml.js";
 
 const button = document.createElement('button');
 button.id = 'theme-switch';
